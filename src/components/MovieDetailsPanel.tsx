4d00e91e299f2c9d0a9cbb5d2d4ec01e1b066fc6
Andromeda08 <vkovacsbalazs26@gmail.com>
2023-04-09 02:57:45 +0200

    4

--- src/components/MovieDetailsPanel.tsx
+++ src/components/MovieDetailsPanel.tsx
@@ -3,38 +3,25 @@ import { useStateContext,  } from '@/context/StateContext';
 import { Genre } from '@/__generated__/graphql';
 import { omdbSearchUrl, wikipediaSearchUrl } from '@/util';
 import { FaLink } from 'react-icons/fa';
+import axios from 'axios';
 
 export const MovieDetailsPanel: React.FC = () => {
   const { selectedMovie: m } = useStateContext();
 
-  const [ links, setLinks ] = useState<{ wp?: string, imdb?: string }>();
+  const [ wiki, setWiki ] = useState<string>();
+  const [ imdb, setImdb ] = useState<string>();
+  const [ poster, setPoster ] = useState<string>();
   const releaseDate = new Date(m!.releaseDate);
 
   const fetchWikipediaAndImdb = async () => {
-    const wpRes = await fetch(wikipediaSearchUrl(m!.name), {
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    }).then(res => res.json())
-    .then(data => {
-      setLinks({
-        wp: data[3],
-        imdb: links?.imdb,
-      })
-    })
+    const wpRes = await axios.get(wikipediaSearchUrl(m!.name))
+    .then(res => setWiki(res.data[3]))
     .catch(err => console.log(err));
 
-    const omdbRes = await fetch(omdbSearchUrl(m!.name), {
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-      }
-    }).then(res => res.json())
-    .then(data => {
-      setLinks({
-        wp: links?.wp,
-        imdb: `https://imdb.com/title/${ data.imdbID }`,
-      })
+    const omdbRes = await axios.get(omdbSearchUrl(m!.name))
+    .then(res => {
+      setPoster(res.data.Poster);
+      setImdb(`https://imdb.com/title/${res.data.imdbID}`)
     })
     .catch(err => console.log(err));
   }
@@ -48,7 +35,7 @@ export const MovieDetailsPanel: React.FC = () => {
     { m && (
       <header>
         <div className='poster'>
-          
+          <img src={ poster } alt={`Poster for ${ m!.name }`} />
         </div>
         <div>
           <h1 className='title'>{ m.name }</h1>
@@ -56,14 +43,14 @@ export const MovieDetailsPanel: React.FC = () => {
           <div className='genres'>
           { m.genres.map((g: Genre) => (<span key={g.id} className='badge'>{ g.name }</span>)) }
           </div>
-          <div>
+          <div className='overview'>
             <h3>Overview</h3>
             <p>{ m.overview }</p>
           </div>
-          <div>
-            <p>{ links?.wp }</p>
-            <p>{ links?.imdb }</p>
-          </div>
+          <ul className='links'>
+            { wiki ? <a href={ wiki } target='_blank'><FaLink /> Wikipedia</a> : <></> }
+            { imdb ? <a href={ imdb } target='_blank'><FaLink /> IMDb</a> : <></> }
+          </ul>
         </div>
       </header>
     )}
